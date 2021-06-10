4f5ecbf5a91126f76a2562d02151b103337d6c6d
kimseongmin0301 <pose1358@naver.com>
2021-06-10 10:54:59 +0900

    그래픽유저인터페이스

--- src/MyGUI.java
+++ src/MyGUI.java
@@ -6,8 +6,8 @@ import javax.swing.event.*;
 public class MyGUI extends JFrame {
 	public MyGUI() {
 		//그래픽 유저 인터페이스
-		this.setTitle("내가 만드는 첫번째 GUI 예제");//sork aksemsms cjtqjsWo GUI dPwp
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //vmfpdlarhk gkaRp vmfhrmfoa whdfy
+		this.setTitle("내가 만드는 첫번째 GUI 예제");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //프레임과 함께 끈다
 		Container cp = this.getContentPane();
 		cp.setBackground(Color.ORANGE);
 		JButton btn = new JButton("ok");
@@ -17,14 +17,14 @@ public class MyGUI extends JFrame {
 				
 		
 		this.setSize(400, 300);
-		this.setVisible(true); //화면에 띄워라(ghkausdp Emldnjfk)
+		this.setVisible(true); //화면에 띄워라
 		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new MyGUI();
 	}
-	class MyActionListner implements ActionListener{  //akdntmfh zmfflrgoTdmfEo
+	class MyActionListner implements ActionListener{  //마우스로 클릭했을때
 		public void actionPerformed(ActionEvent e) {
 			JButton b = (JButton)e.getSource();
 			if(b.getText().equals("ok"))
